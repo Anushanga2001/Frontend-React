42c0752d720e2f32d9ec695104bd3abbf654b18c
Anushanga2001 <anushanga2001@gmail.com>
2025-08-31 18:38:47 +0530

    Layout setup is completed

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 
 import Header from './components/main/Header'
@@ -6,14 +5,12 @@ import Body from "./components/main/Body"
 import Footer from "./components/main/Footer"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className='parent'>
+    <>
       <div className='head'><Header /></div>
       <div className='body'><Body /></div>
       <div className='footer'><Footer /></div>
-    </div>
+    </>
   )
 }
 
--- my-react-app/src/components/main/Header.tsx
+++ my-react-app/src/components/main/Header.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-import { Globe, Menu } from "lucide-react";
 import "./header.css";
 
 function Header() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="parent-header">
       <div className="left">
@@ -12,10 +8,8 @@ function Header() {
       </div>
       <div className="right">
         <div className="lang">
-          <Globe size={18} />
           <span>EN</span>
         </div>
-        <Menu size={22} className="menu" />
       </div>
     </div>
   );

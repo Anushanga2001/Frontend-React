f7d48c88cda05d186b97e2ed24a42f3821a3e708
Anushanga2001 <anushanga2001@gmail.com>
2025-08-31 19:18:26 +0530

    Base completed

--- my-react-app/src/components/main/Header.tsx
+++ my-react-app/src/components/main/Header.tsx
@@ -1,9 +1,11 @@
 import "./header.css";
+import logo from '../../assets/image.png';
 
 function Header() {
   return (
     <div className="parent-header">
       <div className="left">
+        <div className="image1"><img src={logo} alt="Logo"/></div>
         <span className="brand">New Emarald Frinter</span>
       </div>
       <div className="right">

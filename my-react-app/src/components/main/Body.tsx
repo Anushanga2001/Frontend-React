8b73816797bbe03236645ab1a0a353fbbe04f193
Anushanga2001 <anushanga2001@gmail.com>
2025-08-31 19:20:20 +0530

    Fixed suggested issues

--- my-react-app/src/components/main/Body.tsx
+++ my-react-app/src/components/main/Body.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-
 function Body() {
-  const [count, setCount] = useState(0)
-
   return (
     <h1>Hi Body</h1>
   )
--- my-react-app/src/components/main/Footer.tsx
+++ my-react-app/src/components/main/Footer.tsx
@@ -1,8 +1,4 @@
-import { useState } from 'react'
-
 function Footer() {
-  const [count, setCount] = useState(0)
-
   return (
     <h1>Hi Footer</h1>
   )
